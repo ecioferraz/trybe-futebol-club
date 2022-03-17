2130a8748ede6d446f7a828baffdff5897d14770
Écio Ferraz <ecioferraz@hotmail.com>
2022-03-17 19:23:37 -0300

    remove console.log

--- app/backend/src/database/services/Login.ts
+++ app/backend/src/database/services/Login.ts
@@ -8,7 +8,7 @@ const login = async ({ email, password }: ILogin) => {
   const user = await User.findOne({ where: { email } });
 
   if (!user) return { code: StatusCode.UNAUTHORIZED, error: 'Email is not invalid' };
-  console.log(password, user.password);
+
   const isNotValid = bcrypt.compareSync(password, user.password);
 
   if (isNotValid) return { code: StatusCode.UNAUTHORIZED, error: 'Password is not valid' };
