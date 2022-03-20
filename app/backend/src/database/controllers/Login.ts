daf341d0e3570de69740f47826f8a6f0055b8ca3
Écio Ferraz <ecioferraz@hotmail.com>
2022-03-20 10:57:26 -0300

    class controller and service

--- app/backend/src/database/controllers/Login.ts
+++ app/backend/src/database/controllers/Login.ts
@@ -3,35 +3,32 @@ import { StatusCode } from '../interfaces';
 import ILogin from '../interfaces/Login';
 import LoginService from '../services/Login';
 
-const login = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const userInfo: ILogin = req.body;
+export default class LoginController {
+  public static async login(req: Request, res: Response, next: NextFunction) {
+    try {
+      const userInfo: ILogin = req.body;
 
-    const user = await LoginService.login(userInfo);
+      const user = await LoginService.login(userInfo);
 
-    if (user.message) return next(user);
+      if (user.message) return next(user);
 
-    return res.status(StatusCode.OK).json(user);
-  } catch (error) {
-    console.log(error);
-    next(error);
+      return res.status(StatusCode.OK).json(user);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
   }
-};
 
-const getRole = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const token = req.headers.authorization as string;
+  public static getRole(req: Request, res: Response, next: NextFunction) {
+    try {
+      const token = req.headers.authorization as string;
 
-    const role = await LoginService.getRole(token);
+      const role = LoginService.getRole(token);
 
-    return res.status(StatusCode.OK).json(role);
-  } catch (error) {
-    console.log(error);
-    next(error);
+      return res.status(StatusCode.OK).json(role);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
   }
-};
-
-export default {
-  login,
-  getRole,
-};
+}
--- app/backend/src/database/services/Login.ts
+++ app/backend/src/database/services/Login.ts
@@ -6,34 +6,33 @@ import ILogin from '../interfaces/Login';
 import User from '../models/User';
 import IVerified from '../interfaces/User';
 
-const login = async ({ email, password }: ILogin) => {
-  const foundUser = await User.findOne({ where: { email } });
+export default class LoginService {
+  public static async login({ email, password }: ILogin) {
+    const foundUser = await User.findOne({ where: { email } });
 
-  if (!foundUser) return { code: StatusCode.UNAUTHORIZED, message: 'Incorrect email or password' };
+    if (!foundUser) {
+      return { code: StatusCode.UNAUTHORIZED, message: 'Incorrect email or password' };
+    }
 
-  const isValid = bcrypt.compareSync(password, foundUser.password);
+    const isValid = bcrypt.compareSync(password, foundUser.password);
 
-  if (!isValid) return { code: StatusCode.UNAUTHORIZED, message: 'Incorrect email or password' };
+    if (!isValid) return { code: StatusCode.UNAUTHORIZED, message: 'Incorrect email or password' };
 
-  const user = {
-    id: foundUser.id,
-    username: foundUser.username,
-    role: foundUser.role,
-    email,
-  };
+    const user = {
+      id: foundUser.id,
+      username: foundUser.username,
+      role: foundUser.role,
+      email,
+    };
 
-  const token = createToken(user);
+    const token = createToken(user);
 
-  return { user, token };
-};
+    return { user, token };
+  }
 
-const getRole = async (token: string) => {
-  const { role } = jwt.verify(token, jwtSecret) as IVerified;
+  public static getRole(token: string) {
+    const { role } = jwt.verify(token, jwtSecret) as IVerified;
 
-  return role;
-};
-
-export default {
-  login,
-  getRole,
-};
+    return role;
+  }
+}
