5f7cbb5944e833d8b07b496b1cda1f0260396338
Écio Ferraz <ecioferraz@hotmail.com>
2022-03-23 19:00:19 -0300

    req 29 and 30

--- app/backend/src/database/services/Leaderboard.ts
+++ app/backend/src/database/services/Leaderboard.ts
@@ -3,22 +3,37 @@ import Club from '../models/Club';
 import Leaderboards from '../utils/Leaderboards';
 
 export default class LeaderboardService {
+  private static sortLeaderboard = (leaderboard: ILeaderboard[]): ILeaderboard[] =>
+    leaderboard.sort((a, b) => {
+      if (a.totalPoints < b.totalPoints) return 1;
+      if (a.totalPoints > b.totalPoints) return -1;
+      if (a.totalVictories < b.totalVictories) return 1;
+      if (a.totalVictories > b.totalVictories) return -1;
+      if (a.goalsBalance < b.goalsBalance) return 1;
+      if (a.goalsBalance > b.goalsBalance) return -1;
+      if (a.goalsFavor < b.goalsFavor) return 1;
+      if (a.goalsFavor > b.goalsFavor) return -1;
+      if (a.goalsOwn < b.goalsOwn) return 1;
+      if (a.goalsOwn > b.goalsOwn) return -1;
+      return 0;
+    });
+
   public static getAll = async (): Promise<ILeaderboard[]> => {
     const clubs = await Club.findAll();
 
-    const leaderboards = await Promise.all(clubs.map(async (club) => ({
+    const leaderboard = await Promise.all(clubs.map(async (club) => ({
       name: club.clubName,
-      totalPoints: 0,
+      totalPoints: await Leaderboards.getTotalPoints(club),
       totalGames: await Leaderboards.getTotalGames(club),
-      totalVictories: 0,
-      totalDraws: 0,
-      totalLosses: 0,
+      totalVictories: await Leaderboards.getTotalVictories(club),
+      totalDraws: await Leaderboards.getDraws(club),
+      totalLosses: await Leaderboards.getLosses(club),
       goalsFavor: await Leaderboards.getGoalsFavor(club),
       goalsOwn: await Leaderboards.getGoalsOwn(club),
       goalsBalance: await Leaderboards.getGoalsBalance(club),
-      efficiency: 0,
+      efficiency: await Leaderboards.getEfficiency(club),
     })));
 
-    return leaderboards;
+    return this.sortLeaderboard(leaderboard);
   };
 }
--- app/backend/src/database/utils/Leaderboards.ts
+++ app/backend/src/database/utils/Leaderboards.ts
@@ -2,6 +2,8 @@ import Match from '../models/Match';
 import Club from '../models/Club';
 
 export default class Leaderboards {
+  // ref https://sequelize.org/v5/manual/models-usage.html#-code-count--code----count-the-occurrences-of-elements-in-the-database
+
   public static getTotalGames = async (club: Club): Promise<number> =>
     Match.count({ where: { homeTeam: club.id, inProgress: false } });
 
@@ -14,10 +16,43 @@ export default class Leaderboards {
   public static getGoalsBalance = async (club: Club): Promise<number> =>
     await this.getGoalsFavor(club) - await this.getGoalsOwn(club);
 
-  // public static getVictories = async (club: Club): Promise<number> => {
-  //   const goalsFavor = this.getGoalsFavor(club);
-  //   const goalsOwn = this.getGoalsOwn(club);
-  // }
+  private static getMatchGoals = async (club: Club) => Match.findAll({
+    where: { homeTeam: club.id, inProgress: false },
+    attributes: ['homeTeamGoals', 'awayTeamGoals'],
+  });
 
-  // public static getEfficiency = async (club: Club) =>
+  public static getTotalVictories = async (club: Club): Promise<number> => {
+    const matchGoals = await this.getMatchGoals(club);
+
+    return matchGoals.reduce((acc, match) => {
+      if (match.homeTeamGoals > match.awayTeamGoals) return acc + 1;
+      return acc;
+    }, 0);
+  };
+
+  public static getLosses = async (club: Club): Promise<number> => {
+    const matchGoals = await this.getMatchGoals(club);
+
+    return matchGoals.reduce((acc, match) => {
+      if (match.homeTeamGoals < match.awayTeamGoals) return acc + 1;
+      return acc;
+    }, 0);
+  };
+
+  public static getDraws = async (club: Club): Promise<number> => {
+    const matchGoals = await this.getMatchGoals(club);
+
+    return matchGoals.reduce((acc, match) => {
+      if (match.homeTeamGoals === match.awayTeamGoals) return acc + 1;
+      return acc;
+    }, 0);
+  };
+
+  public static getTotalPoints = async (club: Club): Promise<number> =>
+    (await this.getTotalVictories(club) * 3) + await this.getDraws(club);
+
+  public static getEfficiency = async (club: Club) =>
+    +((await this.getTotalPoints(club)
+      / (await this.getTotalGames(club) * 3))
+      * 100).toFixed(2);
 }
